326b0b972477271e1dae3e6c94cca79106eea367
Martossy Alex <github@mail.martossy.hu>
2024-07-06 11:44:10 +0200

    Add benchmark categories, extract expensive properties to static fields

--- test/Serilog.Extensions.Formatting.Benchmark/JsonFormatterBenchmark.cs
+++ test/Serilog.Extensions.Formatting.Benchmark/JsonFormatterBenchmark.cs
@@ -1,10 +1,13 @@
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
 using Serilog.Formatting.Json;
 
 namespace Serilog.Extensions.Formatting.Benchmark;
 
 [SimpleJob]
 [MemoryDiagnoser]
+[GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
+[CategoriesColumn]
 public class JsonFormatterBenchmark
 {
     private Exception _exception = null!;
@@ -12,6 +15,15 @@ public class JsonFormatterBenchmark
     private Stream _stream = null!;
     private Stream _stream2 = null!;
     private ILogger _utf8JsonLog = null!;
+    private static readonly DateTime s_propertyValue0 = new(1970, 1, 1);
+    private static readonly dynamic s_propertyValue1 = new { B = new DateTime(2000, 1, 1), C = new[] { 1, 2, 3 } };
+
+    private static readonly Dictionary<string, DateTime> s_propertyValue2 = new()
+    {
+        { "D", new DateTime(2000, 1, 1) },
+        { "E", new DateTime(2000, 1, 1) },
+        { "F", new DateTime(2000, 1, 1) },
+    };
 
     [GlobalSetup]
     public void Setup()
@@ -34,51 +46,46 @@ public class JsonFormatterBenchmark
         _stream2.Dispose();
     }
 
+    [BenchmarkCategory("EmitLogEvent")]
     [Benchmark(Baseline = true)]
     public void EmitLogEvent()
     {
         _jsonLog.Information(_exception, "Hello, {Name}!", "World");
     }
 
+    [BenchmarkCategory("EmitLogEvent")]
     [Benchmark]
     public void EmitLogEventUtf8()
     {
         _jsonLog.Information(_exception, "Hello, {Name}!", "World");
     }
 
-    [Benchmark]
+    [BenchmarkCategory("IntProperties")]
+    [Benchmark(Baseline = true)]
     public void IntProperties()
     {
         _jsonLog.Information(_exception, "Hello, {A} {B} {C}!", 1, 2, 3);
     }
 
+    [BenchmarkCategory("IntProperties")]
     [Benchmark]
     public void IntPropertiesUtf8()
     {
         _utf8JsonLog.Information(_exception, "Hello, {A} {B} {C}!", 1, 2, 3);
     }
 
-    [Benchmark]
+    [BenchmarkCategory("ComplexProperties")]
+    [Benchmark(Baseline = true)]
     public void ComplexProperties()
     {
-        _jsonLog.Information(_exception, "Hello, {A} {@B} {C}!", new DateTime(1970, 1, 1),
-            new { B = new DateTime(2000, 1, 1), C = new[] { 1, 2, 3 } }, new Dictionary<string, DateTime>
-            {
-                { "D", new DateTime(2000, 1, 1) },
-                { "E", new DateTime(2000, 1, 1) },
-                { "F", new DateTime(2000, 1, 1) },
-            });
+        _jsonLog.Information(_exception, "Hello, {A} {@B} {C}!", s_propertyValue0, s_propertyValue1, s_propertyValue2);
     }
 
+    [BenchmarkCategory("ComplexProperties")]
     [Benchmark]
     public void ComplexPropertiesUtf8()
     {
-        _utf8JsonLog.Information(_exception, "Hello, {A} {@B} {C}!", new DateTime(1970, 1, 1),
-            new { B = new DateTime(2000, 1, 1), C = new[] { 1, 2, 3 } }, new Dictionary<string, DateTime>
-            {
-                { "D", new DateTime(2000, 1, 1) },
-                { "E", new DateTime(2000, 1, 1) },
-                { "F", new DateTime(2000, 1, 1) },
-            });
+        _utf8JsonLog.Information(_exception, "Hello, {A} {@B} {C}!", s_propertyValue0, s_propertyValue1,
+            s_propertyValue2);
     }
 }
